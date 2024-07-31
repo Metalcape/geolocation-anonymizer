61e2314ca457124fad98d787658c57f0f011da21
Metalcape <dario.capecchi.m@gmail.com>
2024-07-31 17:01:13 +0200

    Add extern bindings for C++

--- lib/build.sh
+++ lib/build.sh
@@ -3,6 +3,7 @@
 # Build library
 g++ -c -fPIC -g bfv.cpp -o libbfv.o -I/usr/local/include/SEAL-4.1 -lseal-4.1
 g++ -shared libbfv.o libbfv.h -o libbfv.so -I/usr/local/include/SEAL-4.1 -lseal-4.1
+g++ -shared libbfv.o libbfv_extern.h -o libbfv_extern.so -I/usr/local/include/SEAL-4.1 -lseal-4.1
 
 # Build main for testing
 g++ -g main.cpp ./libbfv.so -I/usr/local/include/SEAL-4.1 -lseal-4.1 -o main.out
