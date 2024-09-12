92251035f9ecd1985ac2b8754d42150d4a9ce755
Metalcape <dario.capecchi.m@gmail.com>
2024-09-12 18:22:05 +0200

    Enable polynomial benchmark

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -2,8 +2,8 @@
 
 ./main.out --type=st --benchmark_out=results/cpu_single_threaded.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=mt --benchmark_out=results/cpu_multi_threaded.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-# ./main.out --type=poly --benchmark_out=results/cpu_multi_threaded_poly.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
+./main.out --type=poly --benchmark_out=results/cpu_multi_threaded_poly.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=gpu_st --benchmark_out=results/gpu_single_threaded.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=gpu_mt --benchmark_out=results/gpu_multi_threaded.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=gpu_range --benchmark_out=results/gpu_range.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-# ./main.out --type=gpu_poly --benchmark_out=results/gpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
+./main.out --type=gpu_poly --benchmark_out=results/gpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
