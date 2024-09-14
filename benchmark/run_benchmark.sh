b4f52f85adbdbdbbaeaf5e4da77a5d616a467e97
Metalcape <dario.capecchi.m@gmail.com>
2024-09-14 12:41:06 +0200

    Change benchmark unit

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -5,7 +5,7 @@
 ./main.out --type=gpu_enc --benchmark_out=results/gpu_encrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=gpu_dec --benchmark_out=results/gpu_decrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=st --benchmark_out=results/cpu_single_threaded.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
-./main.out --type=mt --benchmark_out=results/cpu_multi_threaded.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=mt --benchmark_out=results/cpu_multi_threaded.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=poly --benchmark_out=results/cpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=gpu --benchmark_out=results/gpu.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=gpu_range --benchmark_out=results/gpu_range.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
