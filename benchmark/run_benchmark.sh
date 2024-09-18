9f901f340f7edcba0290a75ee15a2959e1bce9ff
Metalcape <dario.capecchi.m@gmail.com>
2024-09-18 10:00:11 +0200

    Add new polynomial benchmarks

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-./main.out --type=cpu_enc --benchmark_out=results/cpu_encrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-./main.out --type=cpu_dec --benchmark_out=results/cpu_decrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-./main.out --type=gpu_enc --benchmark_out=results/gpu_encrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-./main.out --type=gpu_dec --benchmark_out=results/gpu_decrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=cpu_encode --benchmark_out=results/cpu_encode.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=cpu_decode --benchmark_out=results/cpu_decode.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=gpu_encode --benchmark_out=results/gpu_encode.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=gpu_decode --benchmark_out=results/gpu_decode.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=cpu_encrypt --benchmark_out=results/cpu_encrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=cpu_decrypt --benchmark_out=results/cpu_decrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=gpu_encrypt --benchmark_out=results/gpu_encrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
+./main.out --type=gpu_decrypt --benchmark_out=results/gpu_decrypt.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=st --benchmark_out=results/cpu_single_threaded.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=mt --benchmark_out=results/cpu_multi_threaded.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
-./main.out --type=poly --benchmark_out=results/cpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
 ./main.out --type=gpu --benchmark_out=results/gpu.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
 ./main.out --type=gpu_range --benchmark_out=results/gpu_range.json --benchmark_out_format=json --benchmark_time_unit=ms --benchmark_repetitions=20
-./main.out --type=gpu_poly --benchmark_out=results/gpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=20
-
+./main.out --type=gpu_poly --benchmark_out=results/gpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=10
+./main.out --type=poly --benchmark_out=results/cpu_polynomial.json --benchmark_out_format=json --benchmark_time_unit=s --benchmark_repetitions=10
